2062cdb84dcce0ebf903a2092fd8a949714f5ea7
Marc Abramowitz <marc@marc-abramowitz.com>
2012-04-07 08:42:19 -0700

    Use my gelf-php fork with namespace support

--- test.php
+++ test.php
@@ -1,15 +1,11 @@
 <?php
 
-define('GELF_PHP_DIR', dirname(__FILE__) . '/vendor/graylog2/gelf-php');
+require __DIR__ . "/vendor/.composer/autoload.php";
 
-// Someday we can use the following if gelf-php uses namespaces and is PSR-0 
-// compliant.
-// require dirname(__FILE__) . "/vendor/.composer/autoload.php";
+use Gelf\Message;
+use Gelf\MessagePublisher;
 
-require GELF_PHP_DIR . '/GELFMessage.php';
-require GELF_PHP_DIR . '/GELFMessagePublisher.php';
-
-$message = new GELFMessage();
+$message = new Message();
 $message->setShortMessage('something is broken.');
 $message->setFullMessage("lol full message!");
 $message->setHost('somehost');
@@ -19,7 +15,7 @@ $message->setLine(1337);
 $message->setAdditional("something", "foo");
 $message->setAdditional("something_else", "bar");
 
-$publisher = new GELFMessagePublisher('172.16.22.30');
+$publisher = new MessagePublisher('172.16.22.30');
 $publisher->publish($message);
 
 print "\$publisher = " . print_r($publisher, true) . "\n";
