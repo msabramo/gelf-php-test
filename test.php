4be67d5d823fb5ecceda4da3eda78b2bb62b943d
Marc Abramowitz <marc@marc-abramowitz.com>
2012-04-27 11:07:39 -0700

    Use Composer's autoload.php instead of a bunch of manual requires.

--- test.php
+++ test.php
@@ -1,13 +1,6 @@
 <?php
 
-define('GELF_PHP_DIR', dirname(__FILE__) . '/vendor/graylog2/gelf-php');
-
-// Someday we can use the following if gelf-php uses namespaces and is PSR-0 
-// compliant.
-// require dirname(__FILE__) . "/vendor/.composer/autoload.php";
-
-require GELF_PHP_DIR . '/GELFMessage.php';
-require GELF_PHP_DIR . '/GELFMessagePublisher.php';
+require "vendor/autoload.php";
 
 $message = new GELFMessage();
 $message->setShortMessage('something is broken.');
